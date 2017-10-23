239384e2eaf0630458da0ef7118075431f21d4d1
vikashbaria <vikashbaria4@gmail.com>
2017-10-23 11:32:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -13,7 +13,10 @@ require 'scraperwiki/simple_html_dom.php';
 //  This is 1 Page https://www.bbb.org/en/ca/search?accreditedFilter=0&inputText=a&locationLatLng=&locationText=&page=1&sort=Relevance
 
 $alpha = array("a", "b");
-for($page )
+for($page = 1;  $page> count($alpha); $page++)
+{
+echo $alpha[$page];
+}
 
 
 //
